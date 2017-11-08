491fb3de3c2ebb4d41e6684b647643719eb1a991
kolotaev <kolotaev@tut.by>
2017-11-08 21:22:24 +0300

    Change return format for translatePredicate

--- query.go
+++ query.go
@@ -31,9 +31,17 @@ func getField(f string) string {
 	return f
 }
 
-func (q *Query) translatePredicate(predicate query.Predicate) (map[string]interface{}, error) {
+
+// Turns implicit AND of a list of params into an explicit AND-predicate
+func normalizePredicate(predicate query.Predicate) query.Predicate {
+	if len(predicate) > 1 {
+		return query.Predicate{query.And{predicate}}
+	}
+	return predicate
+}
+
+func (q *Query) translatePredicate(predicate query.Predicate) (string, string, []string, error) {
 	var tempKeys []string
-	var b map[string]interface{}
 
 	for _, exp := range predicate {
 		switch t := exp.(type) {
@@ -72,7 +80,7 @@ func (q *Query) translatePredicate(predicate query.Predicate) (map[string]interf
 				for _, v := range t.Values {
 					inKeys = append(inKeys, sKey(q.entityName, t.Field, v))
 				}
-				b[key1] = fmt.Sprintf(`
+				result := fmt.Sprintf(`
 				local %[1]s = %[2]s
 				for x = %[1]s do
 					local ys = redis.call('ZRANGEBYSCORE', '%[3]s', x, x)
@@ -81,11 +89,12 @@ func (q *Query) translatePredicate(predicate query.Predicate) (map[string]interf
 					end
 				end
 				`, var1, makeLuaTable(inKeys), zKey(q.entityName, t.Field), key1)
+				return key1, result, tempKeys, nil
 			} else {
 				for _, v := range t.Values {
 					inKeys = append(inKeys, sKey(q.entityName, t.Field, v))
 				}
-				b[key3] = fmt.Sprintf(`
+				result := fmt.Sprintf(`
 				local %[1]s = %[2]s
 				if next(%[1]s) != nil then
 					redis.call('SADD', '%[3]s', unpack(%[1]s))
@@ -96,6 +105,7 @@ func (q *Query) translatePredicate(predicate query.Predicate) (map[string]interf
 				end
 				redis.call('SINTERSTORE', '%[7]s', '%[3]s', '%[6]s')
 				`, var1, makeLuaTable(inKeys), key1, var2, sKeyLastAll(q.entityName, t.Field), key2, key3)
+				return key3, result, tempKeys, nil
 			}
 		case query.NotIn:
 			key1 := tmpVar()
@@ -111,7 +121,7 @@ func (q *Query) translatePredicate(predicate query.Predicate) (map[string]interf
 					inKeys = append(inKeys, sKey(q.entityName, t.Field, v))
 				}
 				tuples := getRangePairs(inKeys)
-				b[key1] = fmt.Sprintf(`
+				result := fmt.Sprintf(`
 				for x = %[1]s do
 					local ys = redis.call('ZRANGEBYSCORE', '%[2]s', '(' .. x[1], '(' .. x[2])
 					if next(ys) ~= nil then
@@ -119,11 +129,12 @@ func (q *Query) translatePredicate(predicate query.Predicate) (map[string]interf
 					end
 				end
 				`, makeLuaTable(tuples), zKey(q.entityName, t.Field), key1)
+				return key1, result, tempKeys, nil
 			} else {
 				for _, v := range t.Values {
 					inKeys = append(inKeys, sKey(q.entityName, t.Field, v))
 				}
-				b[key3] = fmt.Sprintf(`
+				result := fmt.Sprintf(`
 				local %[1]s = %[2]s
 				if next(%[1]s) != nil then
 					redis.call('SADD', '%[3]s', unpack(%[1]s))
@@ -134,87 +145,96 @@ func (q *Query) translatePredicate(predicate query.Predicate) (map[string]interf
 				end
 				redis.call('SDIFFSTORE', '%[7]s', '%[6]s', '%[3]s')
 				`, var1, makeLuaTable(inKeys), key1, var2, sKeyLastAll(q.entityName, t.Field), key2, key3)
+				return key3, result, tempKeys, nil
 			}
 		case query.Equal:
+			var result string
 			key := tmpVar()
 			var1 := tmpVar()
 			tempKeys = append(tempKeys, key)
 			if isNumeric(t.Value) {
-				b[key] = fmt.Sprintf(`
+				result = fmt.Sprintf(`
 				local %[5]s = redis.call('ZRANGEBYSCORE', '%[2]s', %[3]d, %[4]d)
 				if next(%[5]s) != nil then
 					redis.call('SADD', '%[1]s', unpack(%[5]s))
 				end
 				`, key, zKey(q.entityName, t.Field), t.Value, t.Value, var1)
 			} else {
-				b[key] = fmt.Sprintf(`
+				result = fmt.Sprintf(`
 				local %[3]s = redis.call('SMEMBERS', '%[2]s')
 				if next(%[3]s) != nil then
 					redis.call('SADD', '%[1]s', unpack(%[3]s))
 				end
 				`, key, sKey(q.entityName, t.Field, t.Value), var1)
 			}
+			return key, result, tempKeys, nil
 		case query.NotEqual:
+			var result string
 			key := tmpVar()
 			tempKeys = append(tempKeys, key)
 			if isNumeric(t.Value) {
 				// TODO: check if all keys are deleted?
-				b[key] = fmt.Sprintf(`
+				result = fmt.Sprintf(`
 				redis.call('ZUNIONSTORE', '%s', 1, '%s')
 				redis.call('ZREMRANGEBYSCORE', '%s', %d, %d)
 				`, key, zKey(q.entityName, t.Field), key, t.Value, t.Value)
 			} else {
-				b[key] = fmt.Sprintf(`
+				result = fmt.Sprintf(`
 				 redis.call('SDIFFSTORE', '%s', '%s', '%s')
 				`, key, sIDsKey(q.entityName), sKey(q.entityName, t.Field, t.Value))
 			}
+			return key, result, tempKeys, nil
 		case query.GreaterThan:
 			key := tmpVar()
 			var1 := tmpVar()
 			tempKeys = append(tempKeys, key)
-			b[key] = fmt.Sprintf(`
+			result := fmt.Sprintf(`
 				local %[4]s = redis.call('ZRANGEBYSCORE', '%[2]s', '(%[3]s', '+inf')
 				if next(%[4]s) != nil then
 					redis.call('SADD', '%[1]s', unpack(%[4]s))
 				end
 				`, key, zKey(q.entityName, t.Field), t.Value, var1)
+			return key, result, tempKeys, nil
 		case query.GreaterOrEqual:
 			key := tmpVar()
 			var1 := tmpVar()
 			tempKeys = append(tempKeys, key)
-			b[key] = fmt.Sprintf(`
+			result := fmt.Sprintf(`
 				local %[4]s = redis.call('ZRANGEBYSCORE', '%[2]s', %[3]d, '+inf')
 				if next(%[4]s) != nil then
 					redis.call('SADD', '%[1]s', unpack(%[4]s))
 				end
 				`, key, zKey(q.entityName, t.Field), t.Value, var1)
+			return key, result, tempKeys, nil
 		case query.LowerThan:
 			key := tmpVar()
 			var1 := tmpVar()
 			tempKeys = append(tempKeys, key)
-			b[key] = fmt.Sprintf(`
+			result := fmt.Sprintf(`
 				local %[4]s = redis.call('ZRANGEBYSCORE', '%[2]s', '-inf', '(%[3]s')
 				if next(%[4]s) != nil then
 					redis.call('SADD', '%[1]s', unpack(%[4]s))
 				end
 				`, key, zKey(q.entityName, t.Field), t.Value, var1)
+			return key, result, tempKeys, nil
 		case query.LowerOrEqual:
 			key := tmpVar()
 			var1 := tmpVar()
 			tempKeys = append(tempKeys, key)
-			b[key] = fmt.Sprintf(`
+			result := fmt.Sprintf(`
 				local %[4]s = redis.call('ZRANGEBYSCORE', '%[2]s', '-inf', %[3]d)
 				if next(%[4]s) != nil then
 					redis.call('SADD', '%[1]s', unpack(%[4]s))
 				end
 				`, key, zKey(q.entityName, t.Field), t.Value, var1)
+			return key, result, tempKeys, nil
 		default:
-			return nil, resource.ErrNotImplemented
+			return "", "", nil, resource.ErrNotImplemented
 		}
 	}
-	return b, nil
+	return "", "", tempKeys, nil
 }
 
 func getQuery(q *query.Query) (string, error) {
-	return translatePredicate(q.Predicate)
+	return translatePredicate(normalizePredicate(q.Predicate))
 }
